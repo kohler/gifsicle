1d1d06325d03e3424d6c280e05882a6e464aff52
Eddie Kohler <eddietwo@mit.edu>
1999-08-16 19:30:28 +0000

    1.14.2

--- win32cfg.h
+++ win32cfg.h
@@ -5,7 +5,7 @@
 
 /* Package and version. */
 #define PACKAGE "gifsicle"
-#define VERSION "1.14.1 (Windows)"
+#define VERSION "1.14.2 (Windows)"
 
 /* Define when using the debugging malloc library. */
 /* #undef DMALLOC */
