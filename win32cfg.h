9dbebe8c8e7ab9dda8bf2a2f5dcf0dfd09cec060
Eddie Kohler <eddietwo@mit.edu>
2000-12-13 14:19:38 +0000

    gifsicle Matt Olech fix

--- win32cfg.h
+++ win32cfg.h
@@ -5,7 +5,7 @@
 
 /* Package and version. */
 #define PACKAGE "gifsicle"
-#define VERSION "1.22 (Windows)"
+#define VERSION "1.23 (Windows)"
 
 /* Define when using the debugging malloc library. */
 /* #undef DMALLOC */
