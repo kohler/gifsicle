d20665277b4e766e3d72994548ce931e8ce67cb6
Eddie Kohler <ekohler@gmail.com>
2003-08-10 21:37:52 +0000

    support HAVE_FAKE_INT_TYPES

--- include/lcdf/inttypes.h
+++ include/lcdf/inttypes.h
@@ -13,4 +13,13 @@ typedef u_int32_t uint32_t;
 # endif
 #endif
 
+#ifdef HAVE_FAKE_INT_TYPES
+typedef signed char int8_t;
+typedef unsigned char uint8_t;
+typedef signed short int16_t;
+typedef unsigned short uint16_t;
+typedef signed int int32_t;
+typedef unsigned int uint32_t;
+#endif
+
 #endif
