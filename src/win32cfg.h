b51e188344961355521ef73e3d91171b294816ae
Eddie Kohler <ekohler@gmail.com>
2006-12-30 22:34:27 +0000

    no X => no gifview, not cryptic compile error

--- src/win32cfg.h
+++ src/win32cfg.h
@@ -100,7 +100,7 @@
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.44 (Windows)"
+#define VERSION "1.45 (Windows)"
 
 /* Define if the X Window System is missing or not being used.  */
 #define X_DISPLAY_MISSING 1
