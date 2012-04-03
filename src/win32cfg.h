d8777f60a56a108739dfb0ff0d23c26677d4bd18
Eddie Kohler <ekohler@gmail.com>
2012-04-02 20:00:59 -0400

    Gifsicle to 1.65.

--- src/win32cfg.h
+++ src/win32cfg.h
@@ -100,7 +100,7 @@
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.64 (Windows)"
+#define VERSION "1.65 (Windows)"
 
 /* Define if the X Window System is missing or not being used.  */
 #define X_DISPLAY_MISSING 1
