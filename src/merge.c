a3d10ab0602e8e64a64a74986004a487ebf7500f
Eddie Kohler <ekohler@gmail.com>
2013-05-17 12:18:06 -0400

    Fix license on merge.c.
    Francois Marier noticed this too.

--- src/merge.c
+++ src/merge.c
@@ -2,10 +2,10 @@
    Copyright (C) 1997-2013 Eddie Kohler, ekohler@gmail.com
    This file is part of gifsicle.
 
-   Gifsicle is free software; you can copy, distribute, or alter it at will, as
-   long as this notice is kept intact and this source code is made available.
-   Hypo(pa)thetical commerical developers are asked to write the author a note,
-   which might make his day. There is no warranty, express or implied. */
+   Gifsicle is free software. It is distributed under the GNU Public License,
+   version 2; you can copy, distribute, or alter it at will, as long
+   as this notice is kept intact and this source code is made available. There
+   is no warranty, express or implied. */
 
 #include <config.h>
 #include "gifsicle.h"
