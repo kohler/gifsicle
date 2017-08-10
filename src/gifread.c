81fd7823f6d9c85ab598bc850e40382068361185
Eddie Kohler <ekohler@gmail.com>
2017-08-10 12:11:04 -0400

    Fix use-after-free problems reported in #114.

--- src/gifread.c
+++ src/gifread.c
@@ -827,6 +827,7 @@ read_gif(Gif_Reader *grr, int read_flags,
           goto done;
       else if (!read_image(grr, &gfc, gfi, read_flags)) {
           Gif_RemoveImage(gfs, gfs->nimages - 1);
+          gfi = 0;
           goto done;
       }
 
